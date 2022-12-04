4ddc1ab20629c9687424aca8f4017c42265e7ff9
Nicole Caceres <janenieggcs@gmail.com>
2022-12-04 14:37:12 -0500

    Se agregó algunos archivos

---
+++ Database2022/Database2022/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database2022.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int Nota { get; set; }
+        public bool EstaAprobado { get; set; }
+       
+    }
+}
---
+++ Database2022/Database2022/Services/StudentService.cs
@@ -0,0 +1,53 @@
+﻿using Database2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Database2022.Services
+{
+    public class StudentService
+    {
+        private readonly AppDbContext _context;
+
+        public StudentService() => _context = App.GetContext();
+
+        public bool Create(Student item)
+        {
+            try
+            {
+                //EntityFrameworkCore
+                _context.Students.Add(item);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool CreateRange(List<Student> items)
+        {
+            try
+            {
+                //EntityFrameworkCore
+                _context.Students.AddRange(items);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public List<Student> Get()
+        {
+            return _context.Students.ToList();
+        }
+
+    }
+}
